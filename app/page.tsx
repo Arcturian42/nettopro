6113edbf2bf215929ee8422dfb30c6b524185592
Eligireno Deploy <deploy@eligireno.fr>
2026-04-15 13:24:09 +0000

    fix: refactor calculateur-aides to use force-dynamic pattern

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import { Hero } from "./components/Hero";
 import { CategoryGrid } from "./components/CategoryGrid";
 import { RegionGrid } from "./components/RegionGrid";
-import { Stats } from "./components/Stats";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Stats />
       <CategoryGrid />
       <RegionGrid />
     </>
